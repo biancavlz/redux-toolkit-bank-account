32f1a654b175046817c53e5fbf2e74fc61859116
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-05 14:00:37 +0100

    Initial  commit

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { thunk } from "redux-thunk";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+
+import customerReducer from "./features/customers/customerSlice";
+import accountReducer from "./features/accounts/accountSlice";
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
